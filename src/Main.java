89365b05febae555b1da634d7668f369932e5a13
100167684 <100167684@lubbockisd.net>
2022-09-15 08:34:09 -0500

    Lab Variables Complete??

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,26 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the following data types byte	short		int 	long  float	 double   char    boolean	String
+        byte value = 30;
+        short anotherValue = 1000;
+        int mostPopularValue = 123443;
+        long extremelyBigNum = 400L;
+        float realValue = 12.9365f;
+        double popularRealValue = 1298;
+        char singleCharacter = 'S';
+        boolean lightSwitch = true;
+        String songName = "Flashing Lights";
+
+        // integer variables
+        byte byteOne = 127;
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+    }
+}
